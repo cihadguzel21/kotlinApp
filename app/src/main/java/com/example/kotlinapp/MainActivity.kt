7eb7ef59dbacee38b794f3a083f3b927bc5479a0
Cihad <cihadguzel21@gmail.com>
2022-02-04 23:00:49 +0300

    4. commit

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
         /**
-         * Changes maded for version 3
+         * Changes maded for version 4
          *
          *
          * */
